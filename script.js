ae4f925694b1c37a06e4be53635474871636853b
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-10-23 12:17:07 +0000

    aula01

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+    //document.getElementById("lampada").scr="on.png"
+    console.log("On")
+}
+
+function apagar(){
+    //document.getElementById("lampada").scr="off.png"
+    console.log("Off")
+}
